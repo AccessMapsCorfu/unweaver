07998997920c6f3e5047c10a7cfadbbc50b7a7e7
MnLsVt <manolis277@gmail.com>
2024-08-21 18:03:35 +0300

    removed example and tests to check if the app breaks

--- example/cost-distance.py
+++
@@ -1,5 +0,0 @@
-def cost_fun_generator(G):
-    def cost_fun(u, v, d):
-        return d.get("length", None)
-
-    return cost_fun
--- tests/conftest.py
+++
@@ -1,1 +0,0 @@
-from .fixtures import built_G, built_G_weighted, test_waypoint_nodes
