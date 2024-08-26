20b98d082f6210db3dfebc57173ad2a9a17d3fe7
MnLsVt <manolis277@gmail.com>
2024-08-26 03:08:56 +0300

    changed a constant to check if distance is the problem

--- unweaver/constants.py
+++ unweaver/constants.py
@@ -5,7 +5,7 @@ DB_PATH = "graph.gpkg"
 
 # The rectangular distance (r-tree distance in meters) within to search for
 # nearby edges.
-DWITHIN = 30
+DWITHIN = 1000 #30
 
 # Default database insert/update batch size
 BATCH_SIZE = 1000
